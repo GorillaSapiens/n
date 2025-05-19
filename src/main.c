a7467d412159ffc9576b3a9fd77cc626bb6e9f2f
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-19 02:19:43 +0000

    here we go!

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Declare from parser.y
+extern int yyparse(void);
+extern void register_typename(const char* name, int size);
+
+// Optional: reference to input file
+extern FILE* yyin;
+
+int main(int argc, char** argv) {
+    // Optional: read from file
+    if (argc > 1) {
+        yyin = fopen(argv[1], "r");
+        if (!yyin) {
+            perror(argv[1]);
+            return 1;
+        }
+    }
+
+    // Register built-in types
+    register_typename("int", 4);
+    register_typename("char", 1);
+    register_typename("void", 0);  // If supported
+
+    printf("Parsing...\n");
+    if (yyparse() == 0) {
+        printf("Parse successful.\n");
+    } else {
+        printf("Parse failed.\n");
+    }
+
+    return 0;
+}
