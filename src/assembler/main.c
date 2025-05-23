3d4a245fa2d3ef7b6cc9aabf4c5f9e251b8816eb
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-23 01:47:50 +0000

    well it compiles

--- src/assembler/main.c
+++ src/assembler/main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "symbols.h"
 
 extern int yyparse();
+extern FILE *yyin;
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *infile = fopen(argv[1], "r");
+    if (!infile) {
+        perror("fopen");
+        return 1;
+    }
+
+    yyin = infile;
     init_symbols();
     int ret = yyparse();
+    fclose(infile);
     dump_output();
     return ret;
 }
