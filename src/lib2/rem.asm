2516eca98803de4d4fb2eaeee7782d475ee6406a
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-24 23:06:55 +0000

    more notes

--- src/lib2/rem.asm
+++ src/lib2/rem.asm
@@ -6,3 +6,20 @@
     jsr div_unsigned
     rts
 .endproc
+
+; rem_signed is very similar to div_signed, with one twist:
+; Both do:
+; 
+;     Detect signs of dividend and divisor
+; 
+;     Make both positive (2's complement if negative)
+; 
+;     Call div_unsigned
+; 
+;     Fix sign of result
+; 
+; The twist:
+; 
+;     div_signed fixes the sign of the quotient based on the signs of A and B
+; 
+;     rem_signed must fix the sign of the remainder to match the original dividend only
