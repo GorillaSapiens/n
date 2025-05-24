c80de81fe0f59ca8ba1ce7d1f208ef7eeb04f0b4
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-24 03:51:09 +0000

    bitwise

--- src/compiler/lib2/bitwise.asm
+++ src/compiler/lib2/bitwise.asm
@@ -3,21 +3,50 @@
 ;
 ; Arbitrary-width bitwise logic on buffers
 ; Inputs:
-;   ptr1, ptr2 - inputs
+;   ptr1, ptr2 - input buffers (ptr2 not needed for NOT)
 ;   ptr3 - destination
 ;   X    - byte count
 ; Clobbers: A, Y
 
 .include "zp.inc"
 
+; Zero page locations assumed
+ptr1     = $00
+ptr2     = $02
+ptr3     = $04
+
 .proc and_bytes
+    ldy #0
+@loop:
+    lda (ptr1), y
+    and (ptr2), y
+    sta (ptr3), y
+    iny
+    dex
+    bne @loop
     rts
 .endproc
 
 .proc or_bytes
+    ldy #0
+@loop:
+    lda (ptr1), y
+    ora (ptr2), y
+    sta (ptr3), y
+    iny
+    dex
+    bne @loop
     rts
 .endproc
 
 .proc not_bytes
+    ldy #0
+@loop:
+    lda (ptr1), y
+    eor #$FF
+    sta (ptr3), y
+    iny
+    dex
+    bne @loop
     rts
 .endproc
