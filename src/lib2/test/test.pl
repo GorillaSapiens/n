1301317c31ab838f787a006c44e7e8dee166a01a
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-25 01:15:22 +0000

    moar

--- src/lib2/test/test.pl
+++ src/lib2/test/test.pl
@@ -15,9 +15,47 @@
 $ca65 = "~/cc65/bin/ca65";
 $ld65 = "~/cc65/bin/ld65";
 
+$sed = "sed \"s/^[[:space:]]*\\.proc[[:space:]]\\+\\([a-zA-Z0-9_]\\+\\)/\\1:/\"";
+
 foreach $file (@tests) {
-	`cat test.asm $file > foo.asm`;
-	`$ca65 foo.asm -o foo.o`;
-	`$ld65 foo.o -C sim.cfg -o foo.bin`;
-	./run6502.exp
+
+   @proc = ();
+
+   open FILE, "$file";
+   foreach $line (<FILE>) {
+      if ($line =~ /\.proc/) {
+         $tmp = $line;
+         $tmp =~ s/[\x0a\x0d]//g;
+         $tmp =~ s/^[\s]*\.proc[\s]+//g;
+         $tmp =~ s/[\s]*//g;
+         push @proc, $tmp;
+      }
+   }
+   close FILE;
+
+   foreach $proc (@proc) {
+      print "=== $file $proc\n";
+
+      print "cat test.asm $file | $sed | sed \"s/TARGET/$proc/g\" > foo.asm\n";
+      print `cat test.asm $file | $sed | sed "s/TARGET/$proc/g" > foo.asm`;
+
+      print "$ca65 foo.asm -o foo.o\n";
+      print `$ca65 foo.asm -o foo.o`;
+
+      print "$ld65 foo.o -C sim.cfg -o foo.bin\n";
+      print `$ld65 foo.o -C sim.cfg -o foo.bin`;
+
+      open FILE, ">script.txt";
+      print FILE "width 72\n";
+      print FILE "load foo.bin 0x8000\n";
+      print FILE "registers pc=0x8000\n";
+      print FILE "disassemble 8000:8040\n";
+      print FILE "add_breakpoint 8026\n";
+      print FILE "goto 8000\n";
+      print FILE "mem 8029:8030\n";
+      close FILE;
+
+      print `py65mon --mpu 6502 < script.txt`;
+      exit 0;
+   }
 }
