dc852a88ecd612b2817ea55c8e0de0f3a07fa43a
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-25 17:59:35 +0000

    smarter shift

--- src/lib2/shift.asm
+++ src/lib2/shift.asm
@@ -13,10 +13,11 @@
 ; Clobbers: A, X, Y
 
 .include "nlib.inc"
-n_shift   = $06
-n_byte    = $07
-n_bit     = $08
-bytecount = $09
+; we use ptr3 and ptr4
+n_shift   = $0A
+n_byte    = $0B
+n_bit     = $0C
+bytecount = $0D
 
 .proc lsl1
     ldx size
@@ -123,96 +124,79 @@ fini_asr8:
     rts
 .endproc
 
-; Logical shift left by N bits (N in A)
+; Logical shift left by N bits (N in shift)
 ; Uses lsl8 and lsl1
+.proc shiftN
+    lda shift
+    sta n_shift
+    and #7
+    sta n_bit
+    lda n_shift
+    lsr
+    lsr
+    lsr
+    sta n_byte
+    lda n_bit
+    beq fini1_shiftN   ; label
+loop1_shiftN:          ; label
+    ldx size
+    jsr trampoline1    ; here
+    dec n_bit
+    bne loop1_shiftN   ; label
+fini1_shiftN:          ; label
+    lda n_byte
+    beq fini2_shiftN   ; label
+loop2_shiftN:          ; label
+    ldx size
+    jsr trampoline2
+    dec n_byte
+    bne loop2_shiftN   ; label
+fini2_shiftN:          ; label
+    rts
+trampoline1:
+    jmp (ptr3)
+trampoline8:
+    jmp (ptr4)
+.endproc
+
 .proc lslN
-    sta n_shift
-    and #7
-    sta n_bit
-    lda n_shift
-    lsr
-    lsr
-    lsr
-    sta n_byte
-    lda n_bit
-    beq fini1_lslN   ; label
-loop1_lslN:          ; label
-    ldx size
-    jsr lsl1         ; here
-    dec n_bit
-    bne loop1_lslN   ; label
-fini1_lslN:          ; label
-    lda n_byte
-    beq fini2_lslN   ; label
-loop2_lslN:          ; label
-    ldx size
-    jsr lsl8
-    dec n_byte
-    bne loop2_lslN   ; label
-fini2_lslN:          ; label
-    rts
+    lda #<lsl1
+    sta ptr3
+    lda #>lsl1
+    sta ptr3+1
+
+    lda #<lsl8
+    sta ptr4
+    lda #>lsl8
+    sta ptr4+1
+
+    jmp shiftN
 .endproc
 
-; Logical shift right by N bits (N in A)
-; Uses lsr8 and lsr1
 .proc lsrN
-    sta n_shift
-    lda n_shift
-    lsr
-    lsr
-    lsr
-    sta n_byte
-    lda n_shift
-    and #7
-    sta n_bit
+    lda #<lsr1
+    sta ptr3
+    lda #>lsr1
+    sta ptr3+1
 
-byte_loop_lsrN:
-    lda n_byte
-    beq bit_shifts_lsrN
-    jsr lsr8
-    dec n_byte
-    jmp byte_loop_lsrN
+    lda #<lsr8
+    sta ptr4
+    lda #>lsr8
+    sta ptr4+1
 
-bit_shifts_lsrN:
-    lda n_bit
-    beq done_lsrN
-bit_loop_lsrN:
-    jsr lsr1
-    dec n_bit
-    bne bit_loop_lsrN
-
-done_lsrN:
-    rts
+    jmp shiftN
 .endproc
 
-; Arithmetic shift right by N bits (N in A)
-; Uses asr8 and asr1
 .proc asrN
-    sta n_shift
-    lda n_shift
-    lsr
-    lsr
-    lsr
-    sta n_byte
-    lda n_shift
-    and #7
-    sta n_bit
+    lda #<asr1
+    sta ptr3
+    lda #>asr1
+    sta ptr3+1
 
-byte_loop_asrN:
-    lda n_byte
-    beq bit_shifts_asrN
-    jsr asr8
-    dec n_byte
-    jmp byte_loop_asrN
+    lda #<asr8
+    sta ptr4
+    lda #>asr8
+    sta ptr4+1
 
-bit_shifts_asrN:
-    lda n_bit
-    beq done_asrN
-bit_loop_asrN:
-    jsr asr1
-    dec n_bit
-    bne bit_loop_asrN
-
-done_asrN:
-    rts
+    jmp shiftN
 .endproc
