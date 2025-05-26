fd28910ac6a03c84736bf229c5e48c066954d214
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-26 16:01:48 +0000

    refactor add for library usage

---
+++ src/nlib/add8.s
@@ -0,0 +1,25 @@
+; add8.s - Arbitrary-length and fixed-width addition routines (unsigned/signed)
+;
+; Adds two little-endian integers of length X bytes (in register X).
+; Inputs:
+;   ptr1 - pointer to operand A (low byte at lower address)
+;   ptr2 - pointer to operand B
+;   ptr3 - pointer to destination buffer
+;   size - number of bytes
+; Assumes:
+;   ptr1, ptr2, ptr3 are 2-byte pointers in zero page
+; Clobbers: A, X, Y, status flags
+
+.export add8
+
+; Zero page locations assumed
+.include "nlib.inc"
+
+.proc add8
+    ldy #0
+    clc
+    lda (ptr1), y
+    adc (ptr2), y
+    sta (ptr3), y
+    rts
+.endproc
--- src/nlib/addN.s
+++ src/nlib/addN.s
@@ -1,5 +1,4 @@
-
-; add.asm - Arbitrary-length and fixed-width addition routines (unsigned/signed)
+; addN.s - Arbitrary-length and fixed-width addition routines (unsigned/signed)
 ;
 ; Adds two little-endian integers of length X bytes (in register X).
 ; Inputs:
@@ -11,10 +10,12 @@
 ;   ptr1, ptr2, ptr3 are 2-byte pointers in zero page
 ; Clobbers: A, X, Y, status flags
 
+.export addN
+
 ; Zero page locations assumed
 .include "nlib.inc"
 
-.proc add_unsigned
+.proc addN
     ldx size
     ldy #0            ; Start at offset 0
     clc               ; Clear carry flag
@@ -25,50 +26,5 @@
     iny               ; Next byte
     dex               ; Decrement byte counter
     bne @loop         ; Repeat if not zero
-    rts
-.endproc
-
-; Fixed width (8/16/32-bit) versions that avoid X usage
-
-.proc add8
-    ldy #0
-    clc
-    lda (ptr1), y
-    adc (ptr2), y
-    sta (ptr3), y
-    rts
-.endproc
-
-.proc add16
-    ldy #0
-    clc
-    lda (ptr1), y
-    adc (ptr2), y
-    sta (ptr3), y
-    iny
-    lda (ptr1), y
-    adc (ptr2), y
-    sta (ptr3), y
-    rts
-.endproc
-
-.proc add32
-    ldy #0
-    clc
-    lda (ptr1), y
-    adc (ptr2), y
-    sta (ptr3), y
-    iny
-    lda (ptr1), y
-    adc (ptr2), y
-    sta (ptr3), y
-    iny
-    lda (ptr1), y
-    adc (ptr2), y
-    sta (ptr3), y
-    iny
-    lda (ptr1), y
-    adc (ptr2), y
-    sta (ptr3), y
     rts
 .endproc
