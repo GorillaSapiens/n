e2487fe990dff2855821067f5e3fa50ae5f05b01
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-26 16:16:21 +0000

    reorg

---
+++ src/nlib/bitwise.s
@@ -0,0 +1,62 @@
+; bitwise.s - AND, OR, NOT, XOR
+;
+; Arbitrary-width bitwise logic on buffers
+; Inputs:
+;   ptr1, ptr2 - input buffers (ptr2 not needed for NOT)
+;   ptr3 - destination
+;   size - byte count
+; Clobbers: A, Y
+
+.include "nlib.inc"
+
+.proc bit_andN
+    ldx size
+    ldy #0
+@loop:
+    lda (ptr1), y
+    and (ptr2), y
+    sta (ptr3), y
+    iny
+    dex
+    bne @loop
+    rts
+.endproc
+
+.proc bit_notN
+    ldx size
+    ldy #0
+@loop:
+    lda (ptr1), y
+    eor #$FF
+    sta (ptr3), y
+    iny
+    dex
+    bne @loop
+    rts
+.endproc
+
+.proc bit_orN
+    ldx size
+    ldy #0
+@loop:
+    lda (ptr1), y
+    ora (ptr2), y
+    sta (ptr3), y
+    iny
+    dex
+    bne @loop
+    rts
+.endproc
+
+.proc bit_xorN
+    ldx size
+    ldy #0
+@loop:
+    lda (ptr1), y
+    eor (ptr2), y
+    sta (ptr3), y
+    iny
+    dex
+    bne @loop
+    rts
+.endproc
