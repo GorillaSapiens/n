80894f4aa04e057fb1ff148a5a1d618234efc022
Gorilla Sapiens <embracetheape@gmail.com>
2025-05-29 21:50:25 +0000

    stragglers

---
+++ src/nlib/foo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "nlib.h"
+
+void test2(void(*fn)(void)) {
+   int val1 = 0x1234;
+   int val2 = 0x5678;
+   int val3 = 0x9ABC;
+   int val4 = 0xDEF0;
+
+   nl_size = 1;
+   nl_shift = 0;
+   nl_ptr1 = &val1;
+   nl_ptr2 = &val2;
+   nl_ptr3 = &val3;
+   nl_ptr4 = &val4;
+
+   printf("before: %02x %02x %04x %04x %04x %04x %04x\n", nl_size, nl_shift,
+      val1, val2, val3, val4);
+   fn();
+   printf("after : %02x %02x %04x %04x %04x %04x %04x\n", nl_size, nl_shift,
+      val1, val2, val3, val4);
+}
+
+int main(void) {
+   printf("hello, world\n");
+
+   test2(addN);
+
+   return 0;
+}
---
+++ src/nlib/nlib_zeropage.s
@@ -0,0 +1,19 @@
+.include "nlib.inc"
+
+.segment "ZEROPAGE"
+
+; 16 bytes total
+
+_nl_size:  .res 1 
+_nl_shift: .res 1
+_nl_ptr1:  .res 2
+_nl_ptr2:  .res 2
+_nl_ptr3:  .res 2
+_nl_ptr4:  .res 2
+_nl_tmp1:  .res 1
+_nl_tmp2:  .res 1
+_nl_tmp3:  .res 1
+_nl_tmp4:  .res 1
+_nl_tmp5:  .res 1
+_nl_tmp6:  .res 1
+
