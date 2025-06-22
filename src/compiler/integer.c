5337ec2cd53e89ca1991371005d242baae48944f
Gorilla Sapiens <embracetheape@gmail.com>
2025-06-22 02:02:51 +0000

    it's all about integers

---
+++ src/compiler/integer.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int make_le_decimal(const char *p, unsigned char *target, int size) {
+   int i;
+   int max = 1;
+   int carry;
+
+   memset(target, 0, size);
+
+   while (*p) {
+      if (*p != '_') {
+         carry = 0;
+         for (i = 0; i < max; i++) {
+            carry = target[i] * 10 + carry;
+            target[i] = carry;
+            carry >>= 8;
+         }
+         if (carry) {
+            target[max++] = carry;
+         }
+
+         carry = target[0] + *p - '0';
+         target[0] = carry;
+         carry >>= 8;
+         i = 1;
+         while (i < max && carry) {
+            carry = target[i] + carry;
+            target[i] = carry;
+            carry >>= 8;
+            i++;
+         }
+         if (carry) {
+            target[max++] = carry;
+         }
+      }
+
+      p++;
+   }
+
+   return max;
+}
+
+#ifdef UNIT_TEST
+void test(const char *p) {
+   unsigned char buf[16];
+   char *b = buf;
+   const char *q = p;
+   int i, n;
+   unsigned long desire;
+
+   while (*q) {
+      if (*q != '_') {
+         *b++ = *q++;
+      }
+      else {
+         q++;
+      }
+   }
+   *b = 0;
+
+   desire = strtoul(buf, NULL, 10);
+
+   n = make_le_decimal(p, buf, sizeof(buf));
+   printf("%ld= (%d) ", desire, n);
+
+   for (i = 0; i < n; i++) {
+      printf("%02x:%02lx ", buf[i], (desire >> (8 * i)) & 0xFF);
+   }
+   printf("\n");
+}
+
+void main(void) {
+   test("0");
+   test("1");
+   test("255");
+   test("256");
+   test("1234");
+   test("12345");
+   test("4_294_967_295");
+   test("4_294_967_296");
+}
+#endif
---
+++ src/compiler/integer.h
@@ -0,0 +1,6 @@
+#ifndef _INCLUDE_INTEGER_H_
+#define _INCLUDE_INTEGER_H_
+
+int make_le_decimal(const char *p, unsigned char *target, int size);
+
+#endif
