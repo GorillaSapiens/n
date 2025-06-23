5c0c6a0dda2d91465ff7edcd3c7179233a89ac45
Gorilla Sapiens <embracetheape@gmail.com>
2025-06-23 00:55:48 +0000

    tweak checks

--- src/compiler/check.c
+++ src/compiler/check.c
@@ -15,7 +15,7 @@ static void check_type_decl(ASTNode *tree) {
    }
 
    if (!strcmp(tree->name, "type_decl")) {
-      debug("%s:%s", __FUNCTION__, tree->children[0]->strval);
+      //debug("%s:%s", __FUNCTION__, tree->children[0]->strval);
       bool haveSize = false;
       int size = -1;
       bool haveEndian = false;
@@ -24,7 +24,7 @@ static void check_type_decl(ASTNode *tree) {
          for (ASTNode *list = tree->children[1];
               list != NULL;
               list = list->children[1]) {
-            debug("%s:\t%s", __FUNCTION__, list->children[0]->strval);
+            //debug("%s:\t%s", __FUNCTION__, list->children[0]->strval);
 
             // check for $size, must be nonnegative
             if (!strncmp(list->children[0]->strval, "$size:", 6)) {
@@ -68,7 +68,7 @@ static void check_type_decl(ASTNode *tree) {
          error("[%s:%d.%d] type_decl '%s' missing '$size:' flag",
             tree->file, tree->line, tree->column, tree->children[0]->strval);
       }
-      if (!haveEndian && size != 0) {
+      if (!haveEndian && size > 1) {
          error("[%s:%d.%d] type_decl '%s' missing '$endian:' flag",
             tree->file, tree->line, tree->column, tree->children[0]->strval);
       }
