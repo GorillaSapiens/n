4b02933394d1b16c190a84e09463568d8a87534f
Gorilla Sapiens <embracetheape@gmail.com>
2025-07-25 04:49:11 +0000

    comments

--- src/compiler/set.h
+++ src/compiler/set.h
@@ -4,10 +4,19 @@
 struct Set;
 typedef struct Set Set;
 
+// create a new Set
 Set *new_set(void);
+
+// delete an existing set, freeing all memory
 void del_set(Set *set);
+
+// add a key/value pair to a set.  allocation/free of key & value handled by caller
 int set_add(Set *set, const char *key, const void *value);
+
+// get the value associated with key
 const void *set_get(Set *set, const char *key);
+
+// remove a key from the Set
 void set_rm(Set *set, const char *key);
 
 #endif
