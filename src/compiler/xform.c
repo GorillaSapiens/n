7cda1ef06a11b4bb071347bcd69bd550e69281d5
Gorilla Sapiens <embracetheape@gmail.com>
2025-08-03 23:58:54 +0000

    completed xform for now

--- src/compiler/xform.c
+++ src/compiler/xform.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "ast.h"
 #include "lextern.h"
@@ -54,13 +55,34 @@ static void strappend(char **sp, int *lp, unsigned char byte) {
    (*sp)[*lp] = 0;
 }
 
+static ASTNode *context = NULL;
+
+static void strappend_helper(const char *match, char **sp, int *lp) {
+   for (int i = 0; i < context->count; i++) {
+      ASTNode *item = context->children[i];
+      if (!strcasecmp(match, item->children[0]->strval)) {
+         for (int j = 1; j < item->count; j++) {
+            strappend(sp, lp, strtol(item->children[j]->strval, NULL, 0));
+            return;
+         }
+      }
+   }
+   strappend(sp, lp, 0xFF);
+}
+
 static void strappend_uval(int uval, char **sp, int *lp) {
+   char buf[16];
+   sprintf(buf, "'\\u%04x'", uval);
+   strappend_helper(buf, sp, lp);
 }
 
 static void strappend_esc(char c, char **sp, int *lp) {
+   char buf[16];
+   sprintf(buf, "'\\%c'", c);
+   strappend_helper(buf, sp, lp);
 }
 
-int decode_utf8(const char *s, int *uval) {
+static int decode_utf8(const char *s, int *uval) {
     *uval = 0;
     const unsigned char *us = (const unsigned char *)s;
 
@@ -121,8 +143,13 @@ const char *do_xform(const char *s, const char *name) {
       name = "";
    }
 
-   s++;
-   while (s[1]) {
+   context = pair_get(xforms, name);
+   if (!context) {
+      warning("could not find context '%s'", name ? name : "(null)");
+      return s;
+   }
+
+   while (*s) {
       if (*s == '\\') {
          if (s[1] == 'x') {
             unsigned char byte = fromhex(2, s+2);
@@ -153,4 +180,3 @@ const char *do_xform(const char *s, const char *name) {
 
    return ret;
 }
-
