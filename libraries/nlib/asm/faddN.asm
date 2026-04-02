9a53ddb7f1dd84ebe63e918154b9abdf25f52493
Gorilla Sapiens <embracetheape@gmail.com>
2026-04-01 21:13:19 -0700

    nlib input cleanup

--- libraries/nlib/asm/faddN.asm
+++ libraries/nlib/asm/faddN.asm
@@ -1,9 +1,9 @@
 ; floating-point add/sub core for IEEE-style binary32
 ;
 ; Inputs:
-;   ptr0 - lhs
-;   ptr1 - rhs
-;   ptr2 - destination
+;   ptr0 - lhs (read-only)
+;   ptr1 - rhs (read-only)
+;   ptr2 - destination / lhs workspace
 ;   arg0 - total size in bytes
 ;   arg1 - exponent bit count
 ;
@@ -43,7 +43,7 @@
     ora (ptr0), y
     bne @lhs_nonzero
     lda tmp1
-    bne @swap_and_store_ptr0
+    bne @swap_and_store_result
     ldy #0
     lda (ptr1), y
     iny
@@ -51,9 +51,9 @@
     iny
     ora (ptr1), y
     beq @store_zero
-@swap_and_store_ptr0:
-    jsr @swap_operands
-    jmp @store_ptr0
+@swap_and_store_result:
+    jsr @swap_work_operands
+    jmp @store_result
 @lhs_nonzero:
     lda tmp1
     bne @have_nonzero_operands
@@ -63,7 +63,7 @@
     ora (ptr1), y
     iny
     ora (ptr1), y
-    beq @store_ptr0
+    beq @store_result
 
 @have_nonzero_operands:
     lda tmp2
@@ -74,173 +74,139 @@
     lda tmp0
     cmp tmp1
     bcs @same_sign_base_ready
-    jsr @swap_operands
+    jsr @swap_work_operands
 @same_sign_base_ready:
     lda tmp0
     sec
     sbc tmp1
-    sta tmp4
+    sta tmp5
     beq @same_sign_add
     cmp #24
     bcs @zero_rhs_for_add
 @same_sign_shift_loop:
-    ldy #2
-    lda (ptr1), y
-    lsr
-    sta (ptr1), y
-    dey
-    lda (ptr1), y
-    ror
-    sta (ptr1), y
-    dey
-    lda (ptr1), y
-    ror
-    sta (ptr1), y
-    dec tmp4
+    jsr @rhs_shift_right_one
+    dec tmp5
     bne @same_sign_shift_loop
     beq @same_sign_add
 @zero_rhs_for_add:
-    ldy #0
-    lda #0
-    sta (ptr1), y
-    iny
-    sta (ptr1), y
-    iny
-    sta (ptr1), y
+    jsr @rhs_zero
 @same_sign_add:
     clc
     ldy #0
-    lda (ptr0), y
-    adc (ptr1), y
-    sta (ptr0), y
+    lda (ptr2), y
+    adc arg0
+    sta (ptr2), y
     iny
-    lda (ptr0), y
-    adc (ptr1), y
-    sta (ptr0), y
+    lda (ptr2), y
+    adc arg1
+    sta (ptr2), y
     iny
-    lda (ptr0), y
-    adc (ptr1), y
-    sta (ptr0), y
-    bcc @store_ptr0
+    lda (ptr2), y
+    adc tmp4
+    sta (ptr2), y
+    bcc @store_result
 
     ; shift the carry+mantissa back down to 24 bits
     ldy #2
-    lda (ptr0), y
+    lda (ptr2), y
     ror
-    sta (ptr0), y
+    sta (ptr2), y
     dey
-    lda (ptr0), y
+    lda (ptr2), y
     ror
-    sta (ptr0), y
+    sta (ptr2), y
     dey
-    lda (ptr0), y
+    lda (ptr2), y
     ror
-    sta (ptr0), y
+    sta (ptr2), y
     inc tmp0
     lda tmp0
     cmp #$ff
-    bne @store_ptr0
+    bne @store_result
     ldy #0
     lda #0
-    sta (ptr0), y
+    sta (ptr2), y
     iny
-    sta (ptr0), y
+    sta (ptr2), y
     iny
-    sta (ptr0), y
-    jmp @store_ptr0
+    sta (ptr2), y
+    jmp @store_result
 
 @different_signs:
     jsr @compare_magnitude
     beq @store_zero
     bcs @lhs_mag_ge_rhs
-    jsr @swap_operands
+    jsr @swap_work_operands
 @lhs_mag_ge_rhs:
     lda tmp0
     sec
     sbc tmp1
-    sta tmp4
+    sta tmp5
     beq @different_sign_sub
     cmp #24
     bcs @zero_rhs_for_sub
 @different_sign_shift_loop:
-    ldy #2
-    lda (ptr1), y
-    lsr
-    sta (ptr1), y
-    dey
-    lda (ptr1), y
-    ror
-    sta (ptr1), y
-    dey
-    lda (ptr1), y
-    ror
-    sta (ptr1), y
-    dec tmp4
+    jsr @rhs_shift_right_one
+    dec tmp5
     bne @different_sign_shift_loop
     beq @different_sign_sub
 @zero_rhs_for_sub:
-    ldy #0
-    lda #0
-    sta (ptr1), y
-    iny
-    sta (ptr1), y
-    iny
-    sta (ptr1), y
+    jsr @rhs_zero
 @different_sign_sub:
     sec
     ldy #0
-    lda (ptr0), y
-    sbc (ptr1), y
-    sta (ptr0), y
+    lda (ptr2), y
+    sbc arg0
+    sta (ptr2), y
     iny
-    lda (ptr0), y
-    sbc (ptr1), y
-    sta (ptr0), y
+    lda (ptr2), y
+    sbc arg1
+    sta (ptr2), y
     iny
-    lda (ptr0), y
-    sbc (ptr1), y
-    sta (ptr0), y
+    lda (ptr2), y
+    sbc tmp4
+    sta (ptr2), y
 
     ldy #0
-    lda (ptr0), y
+    lda (ptr2), y
     iny
-    ora (ptr0), y
+    ora (ptr2), y
     iny
-    ora (ptr0), y
+    ora (ptr2), y
     beq @store_zero
 
 @normalize_left:
     ldy #2
-    lda (ptr0), y
+    lda (ptr2), y
     and #$80
-    bne @store_ptr0
+    bne @store_result
     lda tmp0
     cmp #1
     beq @make_subnormal
     ldy #0
-    lda (ptr0), y
+    lda (ptr2), y
     asl
-    sta (ptr0), y
+    sta (ptr2), y
     iny
-    lda (ptr0), y
+    lda (ptr2), y
     rol
-    sta (ptr0), y
+    sta (ptr2), y
     iny
-    lda (ptr0), y
+    lda (ptr2), y
     rol
-    sta (ptr0), y
+    sta (ptr2), y
     dec tmp0
     jmp @normalize_left
 
 @make_subnormal:
     lda #0
     sta tmp0
-    jmp @store_ptr0
+    jmp @store_result
 
-@store_ptr0:
-    ; tmp0 = effective exponent, tmp2 = sign, ptr0 mantissa bytes contain result
+@store_result:
+    ; tmp0 = effective exponent, tmp2 = sign, ptr2 mantissa bytes contain result
     ldy #2
-    lda (ptr0), y
+    lda (ptr2), y
     sta tmp5
     and #$80
     bne @rawexp_from_tmp0
@@ -254,13 +220,7 @@
     lda tmp0
     sta tmp4
 @rawexp_ready:
-    ldy #0
-    lda (ptr0), y
-    sta (ptr2), y
-    iny
-    lda (ptr0), y
-    sta (ptr2), y
-    iny
+    ldy #2
     lda tmp5
     and #$7f
     sta tmp5
@@ -319,18 +279,18 @@
     bcc @rhs_greater
     bne @lhs_greater
     ldy #2
-    lda (ptr0), y
-    cmp (ptr1), y
+    lda (ptr2), y
+    cmp tmp4
     bcc @rhs_greater
     bne @lhs_greater
     dey
-    lda (ptr0), y
-    cmp (ptr1), y
+    lda (ptr2), y
+    cmp arg1
     bcc @rhs_greater
     bne @lhs_greater
     dey
-    lda (ptr0), y
-    cmp (ptr1), y
+    lda (ptr2), y
+    cmp arg0
     rts
 @lhs_greater:
     sec
@@ -341,42 +301,76 @@
     lda #1
     rts
 
-@swap_operands:
-    lda ptr0
-    sta tmp4
-    lda ptr1
-    sta ptr0
-    lda tmp4
-    sta ptr1
+@swap_work_operands:
+    ldy #0
+    lda (ptr2), y
+    sta tmp5
+    lda arg0
+    sta (ptr2), y
+    lda tmp5
+    sta arg0
 
-    lda ptr0+1
-    sta tmp4
-    lda ptr1+1
-    sta ptr0+1
+    iny
+    lda (ptr2), y
+    sta tmp5
+    lda arg1
+    sta (ptr2), y
+    lda tmp5
+    sta arg1
+
+    iny
+    lda (ptr2), y
+    sta tmp5
     lda tmp4
-    sta ptr1+1
+    sta (ptr2), y
+    lda tmp5
+    sta tmp4
 
     lda tmp0
-    sta tmp4
+    sta tmp5
     lda tmp1
     sta tmp0
-    lda tmp4
+    lda tmp5
     sta tmp1
 
     lda tmp2
-    sta tmp4
+    sta tmp5
     lda tmp3
     sta tmp2
-    lda tmp4
+    lda tmp5
     sta tmp3
     rts
 
-@load_lhs:
-    ldy #2
-    lda (ptr0), y
+@rhs_shift_right_one:
+    lda tmp4
+    lsr
     sta tmp4
-    and #$7f
-    sta (ptr0), y
+    lda arg1
+    ror
+    sta arg1
+    lda arg0
+    ror
+    sta arg0
+    rts
+
+@rhs_zero:
+    lda #0
+    sta arg0
+    sta arg1
+    sta tmp4
+    rts
+
+@load_lhs:
+    ldy #0
+    lda (ptr0), y
+    sta (ptr2), y
+    iny
+    lda (ptr0), y
+    sta (ptr2), y
+    iny
+    lda (ptr0), y
+    sta (ptr2), y
+    sta tmp4
     iny
     lda (ptr0), y
     and #$80
@@ -394,17 +388,17 @@
     lda tmp0
     beq @lhs_zero_or_subnormal
     ldy #2
-    lda (ptr0), y
+    lda (ptr2), y
     ora #$80
-    sta (ptr0), y
+    sta (ptr2), y
     rts
 @lhs_zero_or_subnormal:
     ldy #0
-    lda (ptr0), y
+    lda (ptr2), y
     iny
-    ora (ptr0), y
+    ora (ptr2), y
     iny
-    ora (ptr0), y
+    ora (ptr2), y
     beq @load_lhs_done
     lda #1
     sta tmp0
@@ -412,11 +406,15 @@
     rts
 
 @load_rhs:
-    ldy #2
+    ldy #0
+    lda (ptr1), y
+    sta arg0
+    iny
+    lda (ptr1), y
+    sta arg1
+    iny
     lda (ptr1), y
     sta tmp4
-    and #$7f
-    sta (ptr1), y
     iny
     lda (ptr1), y
     and #$80
@@ -434,26 +432,21 @@
 @rhs_exp_ready:
     lda tmp1
     beq @rhs_zero_or_subnormal
-    ldy #2
-    lda (ptr1), y
+    lda tmp4
     ora #$80
-    sta (ptr1), y
+    sta tmp4
     lda #0
     sta tmp5
     rts
 @rhs_zero_or_subnormal:
-    ldy #0
-    lda (ptr1), y
-    iny
-    ora (ptr1), y
-    iny
-    ora (ptr1), y
+    lda arg0
+    ora arg1
+    ora tmp4
     beq @rhs_loaded_zero
     lda #1
     sta tmp1
 @rhs_loaded_zero:
     lda #0
     sta tmp5
-@load_rhs_done:
     rts
 .endproc
