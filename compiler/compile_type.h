8b11d727c17639b2063f7b2c2e4e3e34339a85aa
Gorilla Sapiens <embracetheape@gmail.com>
2026-04-17 13:36:33 -0700

    calve compile_declarator from compile_type

--- compiler/compile_type.h
+++ compiler/compile_type.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include "ast.h"
 #include "compile_internal.h"
+#include "compile_declarator.h"
 
 const ASTNode *type_name_node(const ASTNode *type);
 const char *type_name_from_node(const ASTNode *type);
@@ -62,41 +63,7 @@ const char *type_float_style(const ASTNode *type);
 int type_float_expbits(const ASTNode *type);
 bool has_modifier(ASTNode *node, const char *modifier);
 bool declaration_const_applies_to_object(const ASTNode *modifiers, const ASTNode *declarator);
-const char *missing_argname(int i);
-ASTNode *make_named_pointer_declarator(const char *name);
-const ASTNode *parameter_decl_specifiers(const ASTNode *parameter);
-const ASTNode *parameter_type(const ASTNode *parameter);
-const ASTNode *parameter_declarator(const ASTNode *parameter);
-bool parameter_is_ref(const ASTNode *parameter);
-bool parameter_has_symbol_storage(const ASTNode *parameter);
-int parameter_storage_size(const ASTNode *parameter);
-const char *parameter_name(const ASTNode *parameter, int i);
-bool parameter_is_void(const ASTNode *parameter);
-const ASTNode *unwrap_expr_node(const ASTNode *expr);
 int get_size(const char *type);
-const ASTNode *declarator_value_declarator(const ASTNode *declarator);
-const ASTNode *declarator_name_node(const ASTNode *declarator);
-const char *declarator_name(const ASTNode *declarator);
-const ASTNode *declarator_bitfield_node(const ASTNode *declarator);
-int declarator_bitfield_width(const ASTNode *declarator);
-int declarator_suffix_start_index(const ASTNode *declarator);
-const ASTNode *declarator_parameter_list(const ASTNode *declarator);
-bool declarator_has_parameter_list(const ASTNode *declarator);
-int declarator_pointer_depth(const ASTNode *declarator);
-int declarator_pointer_node_count(const ASTNode *declarator);
-int declarator_function_pointer_depth(const ASTNode *declarator);
-int declarator_array_multiplier_from(const ASTNode *declarator, int start_child);
-int declarator_array_count(const ASTNode *declarator);
-int declarator_first_element_size(const ASTNode *type, const ASTNode *declarator);
-const ASTNode *clone_declarator_variant(const ASTNode *declarator, int new_ptr_depth, int first_array_child);
-const ASTNode *call_adjusted_parameter_declarator(const ASTNode *declarator, bool is_ref);
-void expr_match_signature(ASTNode *expr, Context *ctx, const ASTNode **type_out, const ASTNode **decl_out);
-const ASTNode *function_pointer_declarator_from_callable(const ASTNode *declarator);
-const ASTNode *function_return_declarator_from_callable(const ASTNode *declarator);
-const ASTNode *declarator_after_subscript(const ASTNode *declarator);
-const ASTNode *declarator_after_deref(const ASTNode *declarator);
-const ASTNode *function_return_type(const ASTNode *fn);
-const ASTNode *function_declarator_node(const ASTNode *fn);
 int type_size_from_node(const ASTNode *type);
 int declarator_value_size(const ASTNode *type, const ASTNode *declarator);
 int expr_value_size(ASTNode *expr, Context *ctx);
@@ -104,11 +71,5 @@ bool expr_is_integer_constant_expr(const ASTNode *expr, long long *value_out);
 bool expr_is_untyped_integer_literal(const ASTNode *expr);
 bool integer_literal_is_zero_expr(const ASTNode *expr);
 bool integer_literal_fits_plain_integer_type(const ASTNode *expr, const ASTNode *formal_type, const ASTNode *formal_decl);
-bool declarator_is_plain_value(const ASTNode *declarator);
-bool declarator_array_signature_matches_from(const ASTNode *actual, const ASTNode *formal, int start_child);
-bool declarator_signature_matches(const ASTNode *actual, const ASTNode *formal);
-bool declarator_is_function(const ASTNode *declarator);
-int declarator_array_multiplier(const ASTNode *declarator);
-int declarator_storage_size(const ASTNode *type, const ASTNode *declarator);
 
 #endif
